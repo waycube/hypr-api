1d146d8fe7ec9daef4ac424263f5758bae3182fd
Your Name <you@example.com>
2026-03-02 15:06:34 +0100

    source venv

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ pkill -f "hypr-api.py" >/dev/null 2>&1
 
 cd ~/.local/bin/hypr-api || exit 1
 
+source ~/.venvs/hypr-agent/bin/activate
+
 exec uvicorn hypr-api:app \
   --host 127.0.0.1 \
   --port 8129 \
