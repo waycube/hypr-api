d2f9d6ab5ca80d0364731875bde06480fdc8cf19
Your Name <you@example.com>
2026-03-03 13:42:49 +0100

    absolute path

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@
 pkill -f "hypr-api.py" >/dev/null 2>&1
 
 
-source venv/bin/activate
+source /home/lobiplayer/.scripts/hypr-api/venv/bin/activate
 
 HOST_IP="${HOST_IP:-$(ip route get 1.1.1.1 2>/dev/null | awk '{for(i=1;i<=NF;i++) if($i=="src") {print $(i+1); exit}}')}"
 HOST_IP="${HOST_IP:-$(hostname -I 2>/dev/null | awk '{print $1}')}"
 HOST_IP="${HOST_IP:-127.0.0.1}"
 
-exec uvicorn hypr-api:app \
+exec uvicorn /home/lobiplayer/.scripts/hypr-api/hypr-api:app \
   --host "$HOST_IP" \
   --port 8129 \
   --log-level warning
